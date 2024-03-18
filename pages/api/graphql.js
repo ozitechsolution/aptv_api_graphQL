ce9d2169976b276e7ffbe6d35f99a8d839d06c90
oussama akarkoub <110060436+oussamaozking55@users.noreply.github.com>
2024-03-18 12:53:53 +0000

    Create a GraphQL server pages

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,31 +1,26 @@
-// graphql-server.js
+// pages/api/graphql.js
 import { ApolloServer, gql } from 'apollo-server-micro';
 
-// Define your GraphQL schema
 const typeDefs = gql `
   type Query {
     hello: String
   }
 `;
 
-// Define your resolvers
 const resolvers = {
     Query: {
-        hello: () => 'Hello from GraphQL server!',
+        hello: () => 'Hello from GraphQL!',
     },
 };
 
-// Create Apollo Server instance
-const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-// Export the Apollo Server instance
 export const config = {
     api: {
         bodyParser: false,
     },
 };
+
+await apolloServer.start();
 
 export default apolloServer.createHandler({ path: '/api/graphql' });
